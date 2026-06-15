72d7480c373c857e9a6023ccea21045198c238cb
Janjira6804062617155 <s6804062617155@email.kmutnb.ac.th>
2026-06-15 16:16:06 +0700

    try

---
+++ w7 try1.py
@@ -0,0 +1,33 @@
+"""from math import *
+a=float(input("enter r:"))
+b=(pi*a**2)
+c=round(b,2)
+print(c)"""
+
+"""a=int(input("enter number"))
+if a<0:
+    print("number < 0")"""
+
+"""a=int(input("enter your score:"))
+if a>5000:
+    print("100")
+else: 
+    print(10)"""
+
+"""a=float(input("enter the temperature"))
+if a>80:
+    print("High")
+elif  50<a<80:
+    print("medium")
+else:
+    print("low")"""
+
+a=input("enter the letters")
+if a=='a':
+    print("Grade A")
+elif a=='b':
+    print("Grade B")
+elif a=='c':
+     print("Grade c")
+else:
+    print("Grade D")
---
+++ w8n1.py
@@ -0,0 +1,8 @@
+x=int(input())
+y=int(input())
+c=(y+1)
+t=0
+
+for f in range(x,c):
+    t+=f
+print(t)
